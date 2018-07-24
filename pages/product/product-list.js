5d3543ffd65e0dffc5b1be1835a1fdcfcacb0e72
Shangwei <Shangwei.Huang@telexpress.com>
2018-07-24 14:37:52 +0800

    Product-list stage 1 - 新增 product-list 頁面檔案 - 完整 product-list 頁面架構 - 調整 product-list 頁面樣式 - 調整 app.wxss 對於 container class 的 padding 定義

---
+++ pages/product/product-list.js
@@ -0,0 +1,69 @@
+// pages/product/product-list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    products: [
+      { id: 1, name: 'Car', imgUrl: "https://images.pexels.com/photos/1200458/pexels-photo-1200458.jpeg?auto=compress&cs=tinysrgb&h=350", description: "A luxury car you won't miss it." },
+      { id: 2, name: 'Bike', imgUrl: "https://images.pexels.com/photos/1239460/pexels-photo-1239460.jpeg?auto=compress&cs=tinysrgb&h=350", description: "A durable bike you've never ride." }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
